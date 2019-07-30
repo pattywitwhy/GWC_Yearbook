498847c967a7c3862c356dedfd834eca634ee5eb
pattywitwhy <pattyxhwang@gmail.com>
2019-07-30 11:14:59 -0500

    student added and bycryt started

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Student.create([
-                {first_name: "Abbey", last_name: "Mullen", birthday: "08/09/02", age: 16, school: "Oak Park River Forrest High School", grade: 11, college: "N/A", major: "Computer Science", bio: "I like to ask people what their favorite state of matter is", final_project: "POC of Chicago"}])
