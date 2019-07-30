3de226bff0ac96204fbfc8b35f19c1ecd61a34b2
pattywitwhy <pattyxhwang@gmail.com>
2019-07-30 10:45:54 -0500

    users controller added

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,15 @@
 class Api::UsersController < ApplicationController
+  def create
+    @users = User.new(
+                      name: params[:name],
+                      email: params[:email],
+                      password: params[:password],
+                      password_confirmation: params[:password_confirmation]
+                      )
+    if user.save
+      render json: {message: "User created successfully"}, status: :created
+    else
+      render json: {errors: user.erros.full_messages}, status: :bad_request
+    end
+  end
 end
